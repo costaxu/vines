a1d631631938b901bf5cc3f22c297b1e2c47f2ea
costaxu <xxb.sklse@gmail.com>
2013-10-21 23:23:39 +0800

    Add accident insurance for zhongmin.cn

--- myproject/myproject/spiders/zhongmin.py
+++ myproject/myproject/spiders/zhongmin.py
@@ -9,6 +9,8 @@ import re, time, json
 from datetime import datetime
 
 TRAVEL_TURN_PAGE_URL = "http://www.zhongmin.cn/TravelAsy.asmx/TravelList"
+ACCID_TURN_PAGE_URL = "http://www.zhongmin.cn/accid/AshxFile/AccidentList.ashx"
+
 class ItemParser:
     def ParseTitle(self, hxs):
         title = hxs.select('//span[@class="cp_tit"]/text()').extract()
@@ -34,6 +36,27 @@ class ItemParser:
             return category.replace('\r\n', '').replace(' ','')
         return u''
 
+class AspxItemParser(ItemParser):
+    def ParseTitle(self, hxs):
+        title = hxs.select('//span[@class="cp_tit"]/span/text()').extract()
+        if title:
+            title = title[0]
+            title = title.replace('\r\n', '').replace(' ','')
+            return title
+
+class ItemParserFactory:
+    item_parser = ItemParser()
+    aspx_item_parser = AspxItemParser()
+    url_parser_map = {
+        "ProductDetails.aspx" : aspx_item_parser,
+        "accident\d+.html" : aspx_item_parser,
+    }
+    def CreateItemParser(self, url):
+        for pattern, parser in self.url_parser_map.items():
+            if re.search(pattern, url):
+                return parser
+        return self.item_parser
+
 class UrlMatch:
     def __init__(self, patterns, callback):
         self.m_patterns = patterns
@@ -49,13 +72,6 @@ class UrlMatch:
         if matched:
             return self.m_callback(response)
 
-    
-class TurnPageExtractor:
-    def matches(self, url):
-        return True
-
-        
-
 class ZhongminSpider(BaseSpider):
     name = u'zhongmin'
     domain = u'zhongmin.cn'
@@ -66,27 +82,47 @@ class ZhongminSpider(BaseSpider):
             ]
     start_urls = [
             'http://www.zhongmin.cn/Travel/',
+            'http://www.zhongmin.cn/accid/'
             ]
     
     url_matches = (
         UrlMatch(
             patterns = [
-                    'Travel/Product/Travel.*html',
+                    'Travel/Product/TravelDetailArr.*html',
                     'ProductDetails.aspx',
+                    '/accid/Product/accident\d+.html',
                     ], 
-            callback = "parse_travel_item",
+            callback = "parse_item",
         ), 
         UrlMatch(
             patterns = ['TravelAsy'],
             callback = "parse_travel_asy",
         ),
         UrlMatch(
-            patterns = ['www.zhongmin.cn/Travel/'],
-            callback = "parse_index",
-        )
+            patterns = ['AccidentList.ashx'],
+            callback = "parse_accid_list",
+        ),
+        UrlMatch(
+            patterns = ['www.zhongmin.cn/Travel/$'],
+            callback = "parse_travel_index",
+        ),
+        UrlMatch(
+            patterns = ['www.zhongmin.cn/accid/$'],
+            callback = "parse_accid_index",
+        ),
     )
     crawled_url_set = set()
-    link_extractor = SgmlLinkExtractor()
+
+    valid_url_patterns = [
+        'Travel/Product/TravelDetailArr.*html',
+        '/accid/Product/accident\d+.html'
+        'ProductDetails.aspx',
+        'TravelAsy',
+        'www.zhongmin.cn/Travel/',
+    ]
+
+    link_extractor = SgmlLinkExtractor(allow = valid_url_patterns)
+    item_parser_factory = ItemParserFactory()
 
     def __init__(self, *a, **kw):
         super(ZhongminSpider, self).__init__(*a, **kw)
@@ -116,8 +152,16 @@ class ZhongminSpider(BaseSpider):
                 url = 'http://www.zhongmin.cn/Travel/Product/TravelDetailArr%(Id)s-%(age)sd%(day)s.html' % product
             request_list.append(Request(url = url))
         return request_list
-    
-    def parse_index(self, response):
+
+    def parse_accid_list(self, response):
+        json_object = json.loads(response.body)
+        request_list = []
+        for product in json_object['dataList']:
+            url = 'http://www.zhongmin.cn/accid/Product/accident%s.html' % product['product']['id']
+            request_list.append(Request(url = url))
+        return request_list
+
+    def parse_travel_index(self, response):
         hxs = HtmlXPathSelector(response)
         page_count = hxs.select("//div[@id='pager1']/b/text()").extract()
         if page_count:
@@ -136,10 +180,29 @@ class ZhongminSpider(BaseSpider):
                         }))
             return request_list
 
-    def parse_travel_item(self, response):
+    def parse_accid_index(self, response):
+        hxs = HtmlXPathSelector(response)
+        page_count = hxs.select("//div[@id='pager1']/b/text()").extract()
+        if page_count:
+            page_count = int(page_count[0])
+            request_list = []
+            #self.log("Parse %d pages from %s" % (page_count, response.url))
+            for i in range(1, page_count + 1):
+                request_list.append(Request(url = ACCID_TURN_PAGE_URL, 
+                        method = "POST", 
+                        body = "smallclass=-1&safeguid=-1&procompany=-1&requestpage=%d&orderfield=1&ordertype=1&guidamount=-1&money=-1&period=-1&type=0" % i,
+                        headers = {
+                            "Accept-Encoding": "gzip, deflate",
+                            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+                            "X-Requested-With": "XMLHttpRequest",
+                            "Accept": "*/*",    
+                        }))
+            return request_list
+
+    def parse_item(self, response):
         self.log("********************************************parse_item*************************************** %s " % (response.url))
         hxs = HtmlXPathSelector(response)
-        item_parser = ItemParser()
+        item_parser = self.item_parser_factory.CreateItemParser(response.url)
         url = response.url
         body = hxs.select("/html/body").extract()
         title = item_parser.ParseTitle(hxs)
@@ -151,7 +214,7 @@ class ZhongminSpider(BaseSpider):
             yield MyItem(title = title, 
                     #body = body,
                     url = url, 
-                    clause_html = clause_html, 
+                    #clause_html = clause_html, 
                     brand = brand, 
                     domain = self.domain,
                     category = category,
